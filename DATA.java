9a7d730c2735bb5aefb0acec03098b4c33b9b612
sturmfx <ttunikitashtobert@gmail.com>
2020-04-14 17:29:22 +0300

    Create DATA.java

---
+++ DATA.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+public class DATA
+{
+    public static volatile INPUT input = new INPUT(5);
+    public static volatile ArrayList<WALL> walls = new ArrayList<>();
+    public static volatile ArrayList<BOTS> bots = new ArrayList<>();
+    public static volatile double x = 100;
+    public static volatile double y = 100;
+    public static volatile String username = "sturmfx";
+    public static volatile int radius = 20;
+    public static volatile int width = 1000;
+    public static volatile int height = 500;
+    public static volatile int direction = 5;
+    public static volatile double speed = 1.0;
+}
