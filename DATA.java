a7581e6358eed4e776e0ccbe487e0d28fc310277
sturmfx <ttunikitashtobert@gmail.com>
2020-04-14 17:37:37 +0300

    Update DATA.java

--- DATA.java
+++ DATA.java
@@ -3,9 +3,8 @@ import java.util.ArrayList;
 
 public class DATA
 {
-    public static volatile INPUT input = new INPUT(5);
-    public static volatile ArrayList<WALL> walls = new ArrayList<>();
-    public static volatile ArrayList<BOTS> bots = new ArrayList<>();
+    
+ public static volatile ArrayList<BOTS> bots = new ArrayList<>();
     public static volatile double x = 100;
     public static volatile double y = 100;
     public static volatile String username = "sturmfx";
